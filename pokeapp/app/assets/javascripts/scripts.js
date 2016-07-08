82b3d28e1f8f2f21938b5887ea60dc4d65ab3536
btchae <bchae@nyu.edu>
2016-07-08 16:45:41 -0400

    adding more things

---
+++ pokeapp/app/assets/javascripts/scripts.js
@@ -0,0 +1,9 @@
+// $('body').append('<div id=container></div>');
+// $('container').append('<h1>Welcome to the world of Pokemon!');
+
+// var newUser = function() {
+//   $.ajax({
+
+//   });
+// }
+// $('body').append('<h1>Are you a boy or a girl?</h1>');
